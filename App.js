73972c66802864c720519d8fff542a0e7587a732
sivasathish889 <sivasathish889@gmail.com>
2025-03-02 22:18:23 +0530

    compressed icon change

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
 import StudentRoute from "./Routes/Studentroute";
 import { ToastProvider } from "react-native-toast-notifications";
+import { SplashScreen } from "expo-router";
+
+SplashScreen.preventAutoHideAsync();
+
 
 const App = () => {
   return (
